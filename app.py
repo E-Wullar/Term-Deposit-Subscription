d0d1d8749f461939e090694b8767abee3c207040
E-Wullar <evelyn.wullar@cogniserve.com>
2025-06-23 11:48:35 +0000

    mmm

--- app.py
+++ app.py
@@ -25,3 +25,10 @@ def predict():
         return jsonify({'prediction': int(pred), 'probability': round(prob, 4)})
     except Exception as e:
         return jsonify({'error': str(e)})
+
+
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000
+    app.run(host="0.0.0.0", port=port)
